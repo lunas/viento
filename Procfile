2f1ea911505c43a63cb8aef205e4f57c0201b69b
lunas <lukasnick@gmail.com>
2014-07-13 15:38:46 +0200

    config file for sidekiq

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 redis: redis-server /usr/local/etc/redis.conf
 web: bundle exec unicorn -c ./config/unicorn.rb -E production
-worker: bundle exec sidekiq -e production
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml
 
---
+++ app/models/email.rb
@@ -0,0 +1,27 @@
+class Email
+  include ActionView::Helpers
+
+  attr_reader :username
+  attr_accessor :referer, :message
+
+  def initialize(username, referer, message = nil)
+    @username = username
+    @referer = referer
+    @message = message
+  end
+
+  def self.create(attributes)
+    clean = attributes.inject({}) {|memo,pair| memo[pair.first.to_sym] = ActionController::Base.helpers.sanitize(pair.last); memo}
+    Email.new(clean[:username], clean[:referer], clean[:message])
+  end
+
+  # unused!
+  def deliver
+    UserMailer.feedback_email(@username, @referer, @message).deliver
+  end
+
+  def empty?
+    return @message.empty?
+  end
+
+end
